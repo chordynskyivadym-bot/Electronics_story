29b8a5ea7591fd30aa9b99c06e8071b990043cc8
user <user@DESKTOP-5SD5AN8>
2025-10-16 00:18:08 +0300

    An electronics store with input and output has been created

--- Лабораторна робота/Program.cs
+++ Лабораторна робота/Program.cs
@@ -12,11 +12,75 @@
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("=== Магазин електроніки ===");
             Console.ResetColor();
-            Console.WriteLine($"1. Ноутбук, {valueNotebook}"); 
-            Console.WriteLine($"2. Смартфон, {valueSmartphone}");
-            Console.WriteLine($"3. Планшет, {valueTablet}");
-            Console.WriteLine($"4. Навушники, {valueРeadphone}");
-            Console.WriteLine($"5. Телевізор, {valueTV}");
+            Console.WriteLine($"1. Ноутбук, {valueNotebook}$"); 
+            Console.WriteLine($"2. Смартфон, {valueSmartphone}$");
+            Console.WriteLine($"3. Планшет, {valueTablet}$");
+            Console.WriteLine($"4. Навушники, {valueРeadphone}$");
+            Console.WriteLine($"5. Телевізор, {valueTV} $");
+
+            Console.Write("\nВиберіть кількість ноутбуків: ");
+            int countNotebook = int.Parse(Console.ReadLine());
+            Console.Write("Виберіть кількість смартфонів: ");
+            int countSmartphone = int.Parse(Console.ReadLine());
+            Console.Write("Виберіть кількість планшетів: ");
+            int countTablet = int.Parse(Console.ReadLine());
+            Console.Write("Виберіть кількість навушників: ");
+            int countРeadphone = int.Parse(Console.ReadLine());
+            Console.Write("Виберіть кількість телевізорів: ");
+            int countTV = int.Parse(Console.ReadLine());
+
+            int totalPriceNotebook = valueNotebook * countNotebook;
+            Console.WriteLine($"\nВартість ноутбуків: {totalPriceNotebook} $");
+            int totalPriceSmartphone = valueSmartphone * countSmartphone;
+            Console.WriteLine($"Вартість смартфонів: {totalPriceSmartphone} $");
+            int totalPriceTablet = valueTablet * countTablet;
+            Console.WriteLine($"Вартість планшетів: {totalPriceTablet} $");
+            int totalPriceРeadphone = valueРeadphone * countРeadphone;
+            Console.WriteLine($"Вартість навушників: {totalPriceРeadphone} $");
+            int totalPriceTV = valueTV * countTV;
+            Console.WriteLine($"Вартість телевізорів: {totalPriceTV} $");
+
+            int totalPrice = totalPriceNotebook + 
+                totalPriceSmartphone + 
+                totalPriceTablet + 
+                totalPriceРeadphone + 
+                totalPriceTV;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nЗагальна вартість: {totalPrice} $");
+            Console.ResetColor();
+
+            Random random = new Random();
+            int discount = random.Next(5, 30);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"\nВаша знижка: {discount}%");
+            Console.ResetColor();
+            double discountAmount = totalPrice * (discount / 100.0);
+
+            double finalPrice = totalPrice - discountAmount;
+            finalPrice = Math.Round(finalPrice, 2);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"\nЦіна зі знижкою: {finalPrice} $");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\n=== Ваші деталі покупки ===");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Ноутбуки: {countNotebook} шт. - {totalPriceNotebook} $");
+            Console.WriteLine($"Смартфони: {countSmartphone} шт. - {totalPriceSmartphone} $");
+            Console.WriteLine($"Планшети: {countTablet} шт. - {totalPriceTablet} $");
+            Console.WriteLine($"Навушники: {countРeadphone} шт. - {totalPriceРeadphone} $");
+            Console.WriteLine($"Телевізори: {countTV} шт. - {totalPriceTV} $");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nПіднесення загальної вартості до кореня:");
+            double roundedFinalPrice = Math.Sqrt(finalPrice);
+            roundedFinalPrice = Math.Round(roundedFinalPrice, 2);
+            Console.WriteLine($"Округлена ціна: {roundedFinalPrice} $");
+            Console.ResetColor();
+
+            Console.WriteLine("\nДякуємо за покупку!");
         }
     }
 }
